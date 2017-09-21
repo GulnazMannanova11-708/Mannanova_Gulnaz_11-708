2392800493b914914f2c3caf36d020ef6332bca9
GulnazMannanova11-708 <31692498+GulnazMannanova11-708@users.noreply.github.com>
2017-09-21 18:27:25 +0300

    seminars Ulearn

---
+++ 2017/FALL2017/PR/Expr/Program10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Expr._10_Mannanova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 5 == 0 || i % 3 == 0)//рассматриыавем те числа, которые делятся на 3 и 5
+                {
+                    sum += i;//находим их сумму
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2017/FALL2017/PR/Expr/Program11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Expr._11_Mannanova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int clock = Convert.ToInt32(Console.ReadLine());
+            int minutes = Convert.ToInt32(Console.ReadLine()); 
+            int degrees = minutes * 6;
+            double part = (clock * 30) + minutes * 0.5;//Расчитываем на какую часть сдвинется часовая стрелка от изначального положения
+            if (degrees - part > 0)
+                Console.WriteLine(degrees - part);
+            else
+                Console.WriteLine(part - degrees);
+            Console.ReadKey();
+        }
+    }
+}
